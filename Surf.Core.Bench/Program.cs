67ee256ccf6aecdd96bb9cacd5f20437cef7d5f3
CarlRabbit <carlthedeadrabbit@googlemail.com>
2020-05-01 16:41:29 +0200

    Add test and benchmark dummy projects

---
+++ Surf.Core.Bench/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Surf.Core.Bench
+{
+    public class Md5VsSha256
+    {
+        private const int N = 10000;
+        private readonly byte[] data;
+
+        private readonly SHA256 sha256 = SHA256.Create();
+        private readonly MD5 md5 = MD5.Create();
+
+        public Md5VsSha256()
+        {
+            data = new byte[N];
+            new Random(42).NextBytes(data);
+        }
+
+        [Benchmark]
+        public byte[] Sha256() => sha256.ComputeHash(data);
+
+        [Benchmark]
+        public byte[] Md5() => md5.ComputeHash(data);
+    }
+
+    /// <remarks>
+    /// Using the dotnet tool version seems buggy with an initial test and depends on dotnetcore 2.X
+    /// </remarks>
+    class Program
+    {
+        static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        //         static void Main(string[] args)
+        //         {
+        // static void Main(string[] args) => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        //             Console.WriteLine("Hello World!");
+        //         }
+    }
+}
---
+++ Surf.Core.Test/TestExampleFsCheck.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Xunit;
+using FsCheck;
+using FsCheck.Xunit;
+
+namespace Surf.Core.Test
+{
+    /// <summary>
+    /// Check out https://www.codit.eu/blog/property-based-testing-with-c
+    /// </summary>
+    public class TestExampleFsCHeck
+    {
+
+        public Property TestProp(int x)
+        {
+            int y = Arb.Generate<int>().Sample(1, 1).Head;
+
+            return (y * 2 == Add(y, y)).ToProperty();
+        }
+
+        [Property]
+        public Property TestProp2(int x)
+        {
+            return (Add(1, Add(1, x)) == Add(x, 2)).ToProperty();
+        }
+
+        [Property]
+        public Property TestProp3(int x, int y)
+        {
+            return (Add(x, y) == Add(y, x)).ToProperty();
+        }
+
+        private int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Property]
+        public Property TestProp4(int x, int y)
+        {
+            Func<bool> property = () => Divide(x * y, y) == x;
+            return property.When(y != 0);
+        }
+
+        [Property]
+        public Property TestProp5(int x)
+        {
+            return Prop.Throws<DivideByZeroException, int>(new Lazy<int>(() => Divide(x, 0)));
+        }
+
+        [Property]
+        public Property TestProp6(int x)
+        {
+            return (x * 2 == Add(x, x)).Trivial(x < 0);
+        }
+
+        [Property]
+        public Property TestProp7(int x)
+        {
+            Func<bool> lazy = () => (Add(1, Add(1, x)) == Add(x, 2));
+            return lazy.Classify(x > 10, "Bigger than '10'")
+                .Classify(x < 1000, "Smaller than '1000'");
+        }
+
+        [Property]
+        public Property TestProp8(int x, int y)
+        {
+            return (Add(x, y) == Add(y, x)).Collect("Values together: " + (x + y));
+        }
+
+        [Property]
+        public Property TestProp9(NonNegativeInt x, NonNegativeInt y)
+        {
+            int result = Add(x.Get, y.Get);
+            return
+              (result >= x.Get).Label("Result is bigger than 'x'").And(
+                result >= y.Get).Label("Result is bigger than 'y'");
+        }
+
+        private int Divide(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
